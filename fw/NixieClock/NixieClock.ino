7846a38eb1b8fea473cd528aa776a14363db7a95
cernohorsky <septonex@gmail.com>
2019-04-17 23:55:50 +0200

    GPIOs correction, Added RTC reading (first test)

--- fw/NixieClock/NixieClock.ino
+++ fw/NixieClock/NixieClock.ino
@@ -1,12 +1,20 @@
 /*
 Nixie Clock
 */
+// Date and time functions using a DS3231 RTC connected via I2C and Wire lib
+#include <Wire.h>
+#include "RTClib.h"
 
-int digit1[]={A10,A11,A12,A13,A14,A15,53,52,51,50};
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+
+int digit1[]={A10,A11,A12,A13,A14,A15,52,53,50,51};
 int digit2[]={A0,A1,A2,A3,A4,A5,A6,A7,A8,A9};
-int digit3[]={49,48,47,46,45,44,43,42,41,40};
-int digit4[]={39,38,37,36,35,34,33,32,31,30};
-int digit5[]={29,28,27,26,25,24,23,22,21,20};
+int digit3[]={48,49,46,47,44,45,42,43,40,41};
+int digit4[]={38,39,36,37,34,35,32,33,30,31};
+int digit5[]={28,29,26,27,24,25,22,23,21,20};
 int digit6[]={10,9,8,7,6,5,4,3,2,1};
 
 
@@ -18,20 +26,64 @@ void setup() {
     pinMode(digit4[i], OUTPUT);  
     pinMode(digit5[i], OUTPUT);  
     pinMode(digit6[i], OUTPUT);  
+
+    digitalWrite(digit1[i], LOW);
+    digitalWrite(digit2[i], LOW);
+    digitalWrite(digit3[i], LOW);
+    digitalWrite(digit4[i], LOW);
+    digitalWrite(digit5[i], LOW);
+    digitalWrite(digit6[i], LOW);
   }
-  
+  rtc.begin();
+/*
+  Serial.begin(115200);
+
+  delay(3000); // wait for console opening
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+  */
 }
 
 void loop() {
+  
+  //DateTime now = rtc.now();
+  /*      
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second()%10, DEC);
+    Serial.println();
+*/  
+int s=5;
+    digitalWrite(digit1[s], HIGH);   
+    delay(500);
+    
+    
+    
   for (int i=0;i<=9;i++) {
+    /*
     digitalWrite(digit1[i], HIGH);   
     digitalWrite(digit2[i], HIGH);   
     digitalWrite(digit3[i], HIGH);   
     digitalWrite(digit4[i], HIGH);   
     digitalWrite(digit5[i], HIGH);   
     digitalWrite(digit6[i], HIGH);   
-    delay(300);                       
-    digitalWrite(digit1[i], LOW);
+    delay(1000);                       
+    */
+    //digitalWrite(digit1[i], LOW);
     digitalWrite(digit2[i], LOW);
     digitalWrite(digit3[i], LOW);
     digitalWrite(digit4[i], LOW);
@@ -40,3 +92,5 @@ void loop() {
   }  
   
 }
+
+
